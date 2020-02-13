bdc1caaeb22832a0dc1f7420a497cd2382d5edd6
Randy <randy408@protonmail.com>
2020-02-13 16:43:36 +0100

    ossfuzz: add dictionary, update build script

--- test/ossfuzz.sh
+++ test/ossfuzz.sh
@@ -1,8 +1,6 @@
 # This script is meant to be run by
 # https://github.com/google/oss-fuzz/blob/master/projects/libspectre/Dockerfile
 
-wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs950/ghostscript-9.50.tar.gz
-tar xvzf ghostscript-9.50.tar.gz
 cd ghostscript-9.50
 ./configure
 make -j$(nproc) soinstall
@@ -18,3 +16,8 @@ $CXX $CXXFLAGS $SRC/libspectre/test/spectre_read_fuzzer.c -I. \
     -o $OUT/spectre_read_fuzzer \
     $LIB_FUZZING_ENGINE $SRC/libspectre/libspectre/.libs/libspectre.a \
     $SRC/libspectre/ghostscript-9.50/bin/gs.a
+
+find $SRC/libspectre/ghostscript-9.50 -name "*.ps" | \
+     xargs zip $OUT/spectre_read_fuzzer_seed_corpus.zip
+
+cp $SRC/libspectre/test/postscript.dict $OUT/spectre_read_fuzzer.dict
